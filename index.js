ea5fd5a0829ca136d955df117c5d2aad86a1b153
Karyna Dominguez <karinaminty@gmail.com>
2019-01-25 17:08:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function whisper(string){
 function logShout(string){
   console.log(string.toUpperCase())
 }
-function logWhisper()
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
